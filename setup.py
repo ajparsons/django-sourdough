70d012e4ef92328f7bc56df8f05c3df4e758beb7
Alex Parsons <alex@alexparsons.co.uk>
2016-06-10 10:22:46 +0100

    change version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'useful_inkleby',
   packages = ['useful_inkleby'],
-  version = '0.1',
+  version = '0.2',
   description = 'A set of django and generic tools',
   author = 'Alex Parsons',
   author_email = 'alex@alexparsons.co.uk',
   url = 'https://github.com/ajparsons/useful_inkleby', 
-  download_url = 'https://github.com/ajparsons/useful_inkleby/0.1', 
+  download_url = 'https://github.com/ajparsons/useful_inkleby/0.2', 
   keywords = ['django'], 
   classifiers = [],
 )
